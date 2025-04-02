6e228550cb25b68f9b6132a3e05b7c1f3a1affe5
crL96 <190506813+crL96@users.noreply.github.com>
2025-04-02 12:20:27 +0200

    Change passed down prop to only currentScore instead of the whole list of selectedItems

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import PokemonCard from "./PokemonCard";
 
-export default function PokemonList({selectedPokemons, handleClick}) {
+export default function PokemonList({currentScore, handleClick}) {
     const [indexOrder, setIndexOrder] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
   
     useEffect(() => {
       setIndexOrder(indexOrder => indexOrder.sort(() => 0.5 - Math.random()))
-    }, [selectedPokemons.length]);
+    }, [currentScore]);
   
     const pokemons = [];
     for (let i = 0; i < 12; i++) {
